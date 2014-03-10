00584ae1c1b585a4cf77a30a09cd8d8c915a78e8
Micah Eschbacher <michaeleschbacher@gmail.com>
2014-03-09 21:37:39 -0500

    Bug Fix: No longer need to pass object to Alert

--- src/alerts/main.js
+++ src/alerts/main.js
@@ -1,6 +1,7 @@
 define(['./models/alert','./views/alert_view'], function (Alert, AlertView) {
     return {
         create: function (params) {
+            params = params || {};
             params.model = new Alert;
             return new AlertView(params);
         }
