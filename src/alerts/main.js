db8c2e086555bcb0fed17fa44d4de551a1fcc536
Micah Eschbacher <michaeleschbacher@gmail.com>
2014-03-09 21:39:31 -0500

    Basic JSLinting

--- src/alerts/main.js
+++ src/alerts/main.js
@@ -2,8 +2,8 @@ define(['./models/alert','./views/alert_view'], function (Alert, AlertView) {
     return {
         create: function (params) {
             params = params || {};
-            params.model = new Alert;
+            params.model = new Alert();
             return new AlertView(params);
         }
-    }
+    };
 });
